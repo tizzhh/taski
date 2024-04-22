1770caa3c88b0a826f4bd1fb70126fcf41ff8b1d
Vadim Kuznetsov <darovadraste@gmail.com>
2024-04-22 17:04:14 +0000

    Add sentry, nginx server, gunicorn, tls, uptime

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,24 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from pathlib import Path
+
+sentry_sdk.init(
+    dsn="https://2571a818d21a87b6f471f9a6bfdf5793@o4507131212857344.ingest.de.sentry.io/4507131214757968",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+) 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.90.86', '127.0.0.1', 'localhost', 'yapracttaski2.hopto.org']
 
 
 # Application definition
@@ -103,7 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
